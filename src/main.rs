86f146e36f458ac224e534a4b44e076e55174861
Emoon <gustavo.chicato@usp.br>
2021-07-07 18:57:15 -0300

    Printing to screen via VGA buffer

--- src/main.rs
+++ src/main.rs
@@ -10,9 +10,23 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
+// Define constant string "Hello World!"
+static HELLO: &[u8] = b"Hello World!";
+
 // Don't mangle function's name
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     // Entry point; linker looks for `_start` by default
+    
+    // Define pointer to VGA buffer location in memory
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
     loop {}
 }
