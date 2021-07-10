4fc94962bb48ac2b0190aa823a7410c5d907aed5
Emoon <gustavo.chicato@usp.br>
2021-07-09 21:53:13 -0300

    `Testable` trait to automate test info messages

--- src/test.rs
+++ src/test.rs
@@ -2,16 +2,18 @@
  *  Automated unit tests module.
  */
 
+use core::any::type_name;
 use crate::serial_print;
 use crate::serial_println;
 use crate::qemu;
 
 #[cfg(test)]  // includes test function only for testing
-pub fn runner(tests: &[&dyn Fn()]) {
-    /* Iterate test functions and run them. */
+pub fn runner(tests: &[&dyn Testable]) {
+    /* Iterate test functions and run them.
+        Exit thereafter with successful exit code. */
     serial_println!("Running {} tests", tests.len());
     for test in tests {
-        test();
+        test.run();
     }
     qemu::exit(qemu::ExitCode::Success);
 }
@@ -19,7 +21,22 @@ pub fn runner(tests: &[&dyn Fn()]) {
 #[test_case]  // defines a test case function
 fn trivial_assertion() {
     /* Simply trivial assertion that is always true. */
-    serial_print!("Trivial assertion... ");
-    assert_eq!(2 + 2, 5);
-    serial_println!("[ok]");
+    assert_eq!(2 + 2, 4);
+}
+
+/*---------------------------------------------------------------------------*/
+
+pub trait Testable {
+    /* Trait to be given to testing functions. */
+    fn run(&self) -> ();
+}
+
+impl<T> Testable for T where T: Fn() {
+    fn run(&self) {
+        /* Wrapper runner function. Prints function name,
+            run test and then prints [ok] if successful. */
+        serial_print!("{}...\t", type_name::<T>());
+        self();
+        serial_println!("[ok]");
+    }
 }
