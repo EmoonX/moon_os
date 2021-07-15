e1c425eda8033e1c42903e83c324d42b016e5661
Emoon <gustavo.chicato@usp.br>
2021-07-14 23:02:33 -0300

    Timer interrupt handler (one-time only, for now)

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -10,7 +10,7 @@ use pic8259::ChainedPics;
 use lazy_static::lazy_static;
 
 use crate::gdt;
-use crate::println;
+use crate::{print, println};
 
 /**
  *  Loading and initialization procedures.
@@ -23,25 +23,6 @@ use crate::println;
     // Initializes PICs and enable CPU listening to interrupts
     unsafe { PICS.lock().initialize() };
     interrupts::enable();
-}
-
-lazy_static! {
-    /**
-     *  Interrupt Descriptor Table (IDT).
-     *
-     *  Should be available for the complete program runtime
-     *  (thus `static`) and set up all exception handlers on init.
-     */
-    static ref IDT: InterruptDescriptorTable = {
-        let mut idt = InterruptDescriptorTable::new();
-        idt.breakpoint.set_handler_fn(breakpoint_handler);
-        unsafe {
-            idt.double_fault
-                .set_handler_fn(double_fault_handler)
-                .set_stack_index(gdt::DOUBLE_FAULT_IST_INDEX);
-        }
-        idt
-    };
 }
 
 /// Primary PIC's vector number offset
@@ -58,6 +39,35 @@ pub static PICS: spin::Mutex<ChainedPics> = spin::Mutex::new(
         ChainedPics::new(PIC_1_OFFSET, PIC_2_OFFSET)
     }
 );
+
+/**
+ *  PIC interrupt indices, starting by default at 32.
+ */
+#[repr(u8)]
+enum InterruptIndex {
+    Timer = PIC_1_OFFSET,
+}
+
+lazy_static! {
+    /**
+     *  Interrupt Descriptor Table (IDT).
+     *
+     *  Should be available for the complete program runtime
+     *  (thus `static`) and set up all interrupt handlers on init.
+     */
+    static ref IDT: InterruptDescriptorTable = {
+        let mut idt = InterruptDescriptorTable::new();
+        idt.breakpoint.set_handler_fn(breakpoint_handler);
+        unsafe {
+            idt.double_fault
+                .set_handler_fn(double_fault_handler)
+                .set_stack_index(gdt::DOUBLE_FAULT_IST_INDEX);
+        }
+        idt[InterruptIndex::Timer as usize]
+            .set_handler_fn(timer_interrupt_handler);
+        idt
+    };
+}
 
 /*---------------------------------------------------------------------------*/
 
@@ -86,6 +96,12 @@ extern "x86-interrupt" fn double_fault_handler(
         qemu::exit(qemu::ExitCode::Success);
     }
     panic!("[EXCEPTION] DOUBLE FAULT\n{:#?}", stack_frame);
+}
+
+extern "x86-interrupt" fn timer_interrupt_handler(
+    _stack_frame: InterruptStackFrame)
+{
+    print!(".");
 }
 
 /*---------------------------------------------------------------------------*/
